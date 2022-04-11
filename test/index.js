335716485ed5ed7ac07b49ab07e0f16007b29a95
Herton <d@ntas.cc>
2022-04-10 21:13:36 -0400

    Adding basic test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,22 @@
-import { pass } from 'tap';
+import tap from 'tap';
+import { JsUpdatr } from '../dist/index.js'
 
-pass('this is the beggining of great tests!')
+tap.test('testing JsUpdatr', tap => {
+    const state = {
+        name: 'bob',
+        age: 22
+    }
+    const result = {};
+    JsUpdatr.observe(state);
+    JsUpdatr.run(() => {
+        result['string'] = state.name;
+    })
+    state.name = 'jorge'
+    tap.equal(state.name, result.string)
+
+    JsUpdatr.run(() => {
+        result['number'] = state.age;
+    })
+    tap.equal(state.age, result.number)
+    tap.end()
+})
